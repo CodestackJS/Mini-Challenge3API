fca681eea5b39f048d4a1276cf265758cfc88f85
Jerie See Villanueva <codestackjs007@gmail.com>
2024-05-01 21:20:39 -0700

    added Models folder

--- api/Models/Todo.cs
+++ api/Models/Todo.cs
@@ -9,6 +9,7 @@ namespace api.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+        public string? Description { get; set; }
         public bool Complete { get; set; }
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,11 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
